cbce7cdba828a798389e5c380d8c1e83cadfc005
rafael.ferreira <rafah.26ferreira@hotmail.com>
2020-04-24 18:09:17 -0300

    changin applicationEvent

--- src/main/java/com/dynamic/command/ApplicationStartup.java
+++ src/main/java/com/dynamic/command/ApplicationStartup.java
@@ -1,25 +1,39 @@
 package com.dynamic.command;
 
-import org.springframework.data.mongodb.config.AbstractMongoClientConfiguration;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationEvent;
+import org.springframework.context.ApplicationListener;
 import org.springframework.stereotype.Component;
 
-import com.mongodb.client.MongoClient;
+import com.dynamic.command.kafka.service.KafkaService;
+import com.dynamic.command.mongo.TweetTopicModel;
+import com.dynamic.command.mongo.repository.TweetTopicRepository;
 
 @Component
-public class ApplicationStartup extends AbstractMongoClientConfiguration{
-
-	@Override
-	public MongoClient mongoClient() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	protected String getDatabaseName() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
+public class ApplicationStartup implements ApplicationListener<ApplicationEvent>{
 	
+	private Logger logger = LoggerFactory.getLogger(ApplicationStartup.class.getName());
+	
+	@Autowired
+	private TweetTopicRepository repository;
+	
+	@Autowired
+	private KafkaService kafkaService;
+	
+	@Override
+	public void onApplicationEvent(ApplicationEvent event) {
+		logger.info("Checking for active tops on intialization");
+		List<TweetTopicModel> activeTopics = repository.findActiveTopics("yes");
+		List<String> topics = new ArrayList<String>();
+		for(TweetTopicModel tweetTopic : activeTopics) {
+			topics.add(tweetTopic.getTopicName());
+		}
+		kafkaService.send(topics);
+	}
 
 }
