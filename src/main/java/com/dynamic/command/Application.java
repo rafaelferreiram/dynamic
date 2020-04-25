8467f5149589d6b4c1e1d9d2112587c75770910d
rafael.ferreira <rafah.26ferreira@hotmail.com>
2020-04-25 20:02:02 -0300

    On startup event search for actives

--- src/main/java/com/dynamic/command/Application.java
+++ src/main/java/com/dynamic/command/Application.java
@@ -1,15 +1,25 @@
 package com.dynamic.command;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
 @EnableAsync
 public class Application {
+	
+	@Autowired
+	private ApplicationStartup startup;
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
-
+	
+	@EventListener(ApplicationReadyEvent.class)
+	public void doSomethingAfterStartup() {
+	    startup.onStartup();
+	}
 }
--- src/main/java/com/dynamic/command/ApplicationStartup.java
+++ src/main/java/com/dynamic/command/ApplicationStartup.java
@@ -6,8 +6,6 @@ import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ApplicationEvent;
-import org.springframework.context.ApplicationListener;
 import org.springframework.stereotype.Component;
 
 import com.dynamic.command.kafka.service.KafkaService;
@@ -15,7 +13,7 @@ import com.dynamic.command.mongo.TweetTopicModel;
 import com.dynamic.command.mongo.repository.TweetTopicRepository;
 
 @Component
-public class ApplicationStartup implements ApplicationListener<ApplicationEvent>{
+public class ApplicationStartup {
 	
 	private Logger logger = LoggerFactory.getLogger(ApplicationStartup.class.getName());
 	
@@ -25,8 +23,7 @@ public class ApplicationStartup implements ApplicationListener<ApplicationEvent>
 	@Autowired
 	private KafkaService kafkaService;
 	
-	@Override
-	public void onApplicationEvent(ApplicationEvent event) {
+	public void onStartup() {
 		logger.info("Checking for active tops on intialization");
 		List<TweetTopicModel> activeTopics = repository.findActiveTopics("yes");
 		List<String> topics = new ArrayList<String>();
