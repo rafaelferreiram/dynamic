868003c0bb1676f61817d1c7857c0a418f5c7ad5
rafael.ferreira <rafah.26ferreira@hotmail.com>
2020-04-28 00:55:01 -0300

    configuring cross origin

--- src/main/java/com/dynamic/command/controller/ConfigController.java
+++ src/main/java/com/dynamic/command/controller/ConfigController.java
@@ -2,6 +2,7 @@ package com.dynamic.command.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +12,7 @@ import com.dynamic.command.kafka.producer.dto.response.ConfigResponseDTO;
 
 @RestController
 @RequestMapping("/config")
+@CrossOrigin(origins = "*")
 public class ConfigController {
 	
 	@Autowired
