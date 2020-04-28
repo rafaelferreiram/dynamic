50c3134a9d31a6cae54ded17c5b989afee01aed9
rafael.ferreira <rafah.26ferreira@hotmail.com>
2020-04-28 00:31:15 -0300

    refactoring mongo service

--- src/main/java/com/dynamic/command/mongo/service/MongoService.java
+++ src/main/java/com/dynamic/command/mongo/service/MongoService.java
@@ -1,70 +1,20 @@
 package com.dynamic.command.mongo.service;
 
-import java.util.Date;
 import java.util.List;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Component;
-
 import com.dynamic.command.mongo.TweetTopicModel;
-import com.dynamic.command.mongo.TweetsLogModel;
-import com.dynamic.command.mongo.repository.TweetLogRepository;
-import com.dynamic.command.mongo.repository.TweetTopicRepository;
 
-@Component
-public class MongoService {
+public interface MongoService {
 
-	private Logger logger = LoggerFactory.getLogger(MongoService.class.getName());
+	public List<TweetTopicModel> findAllTopics() ;
 
-	@Autowired
-	private TweetTopicRepository repository;
+	public List<TweetTopicModel> findActiveTopics() ;
 
-	@Autowired
-	private TweetLogRepository logRepository;
+	public TweetTopicModel findByTopicName(String topic) ;
 
-	@Value("${twitter.topic.active}")
-	private String active;
-	
-	public List<TweetTopicModel> findAllTopics() {
-		List<TweetTopicModel> allTopics = repository.findAll();
-		logger.info("Total of " + allTopics.size() + " topics.");
-		return allTopics;
-	}
+	public void update(TweetTopicModel tweetTopic) ;
 
-	public List<TweetTopicModel> findActiveTopics() {
-		List<TweetTopicModel> activeTopics = repository.findActiveTopics(active);
-		logger.info("Total of active topics " + activeTopics.size());
-		return activeTopics;
-	}
+	public void saveNewTopic(String topic) ;
 
-	public TweetTopicModel findByTopicName(String topic) {
-		return repository.findByTopicName(topic);
-	}
-
-	public void update(TweetTopicModel tweetTopic) {
-		try {
-			repository.save(tweetTopic);
-			logger.info("Tweet Topic updated on MongoDB successfully!");
-		} catch (Exception e) {
-			logger.error("Error when updating topic on MongoDB.", e.getMessage());
-		}
-	}
-
-	public void saveNewTopic(String topic) {
-		try {
-			TweetTopicModel tweetTopic = new TweetTopicModel(topic, new Date().toString(), active);
-			repository.save(tweetTopic);
-			logger.info("Tweet Topic saved on MongoDB successfully!\n" + tweetTopic.toString());
-		} catch (Exception e) {
-			logger.error("Error when saving topic on MongoDB.", e.getMessage());
-		}
-	}
-
-	public void saveLog(String log) {
-		TweetsLogModel logModel = new TweetsLogModel(log, new Date().toString());
-		logRepository.save(logModel);
-	}
+	public void saveLog(String log) ;
 }
--- src/main/java/com/dynamic/command/mongo/service/impl/MongoServiceImpl.java
+++ src/main/java/com/dynamic/command/mongo/service/impl/MongoServiceImpl.java
@@ -1,5 +1,71 @@
 package com.dynamic.command.mongo.service.impl;
 
-public class MongoServiceImpl {
+import java.util.Date;
+import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import com.dynamic.command.mongo.TweetTopicModel;
+import com.dynamic.command.mongo.TweetsLogModel;
+import com.dynamic.command.mongo.repository.TweetLogRepository;
+import com.dynamic.command.mongo.repository.TweetTopicRepository;
+import com.dynamic.command.mongo.service.MongoService;
+
+@Component
+public class MongoServiceImpl implements MongoService{
+
+	private Logger logger = LoggerFactory.getLogger(MongoService.class.getName());
+
+	@Autowired
+	private TweetTopicRepository repository;
+
+	@Autowired
+	private TweetLogRepository logRepository;
+
+	@Value("${twitter.topic.active}")
+	private String active;
+	
+	public List<TweetTopicModel> findAllTopics() {
+		List<TweetTopicModel> allTopics = repository.findAll();
+		logger.info("Total of " + allTopics.size() + " topics.");
+		return allTopics;
+	}
+
+	public List<TweetTopicModel> findActiveTopics() {
+		List<TweetTopicModel> activeTopics = repository.findActiveTopics(active);
+		logger.info("Total of active topics " + activeTopics.size());
+		return activeTopics;
+	}
+
+	public TweetTopicModel findByTopicName(String topic) {
+		return repository.findByTopicName(topic);
+	}
+
+	public void update(TweetTopicModel tweetTopic) {
+		try {
+			repository.save(tweetTopic);
+			logger.info("Tweet Topic updated on MongoDB successfully!");
+		} catch (Exception e) {
+			logger.error("Error when updating topic on MongoDB.", e.getMessage());
+		}
+	}
+
+	public void saveNewTopic(String topic) {
+		try {
+			TweetTopicModel tweetTopic = new TweetTopicModel(topic, new Date().toString(), active);
+			repository.save(tweetTopic);
+			logger.info("Tweet Topic saved on MongoDB successfully!\n" + tweetTopic.toString());
+		} catch (Exception e) {
+			logger.error("Error when saving topic on MongoDB.", e.getMessage());
+		}
+	}
+
+	public void saveLog(String log) {
+		TweetsLogModel logModel = new TweetsLogModel(log, new Date().toString());
+		logRepository.save(logModel);
+	}
 }
