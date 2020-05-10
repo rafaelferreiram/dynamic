a6a2b3b55f70c37a29dc8b40bedd9ed75159aa7c
rafael.ferreira <rafah.26ferreira@hotmail.com>
2020-05-10 19:08:41 -0300

    setting up description on Swagger

--- src/main/java/com/dynamic/command/Application.java
+++ src/main/java/com/dynamic/command/Application.java
@@ -9,6 +9,7 @@ import org.springframework.context.event.EventListener;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -17,22 +18,22 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableSwagger2
 @EnableAsync
 public class Application {
-	
+
 	@Autowired
 	private ApplicationStartup startup;
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
-	
+
 	@EventListener(ApplicationReadyEvent.class)
 	public void doSomethingAfterStartup() {
-	    startup.onStartup();
+		startup.onStartup();
 	}
-	
+
 	@Bean
 	public Docket productApi() {
 		return new Docket(DocumentationType.SWAGGER_2).select()
-				.apis(RequestHandlerSelectors.basePackage("com.dynamic.command.controller")).build();
+				.apis(RequestHandlerSelectors.basePackage("com.dynamic.command.controller")).build().apiInfo(new ApiInfo("Dynamic Twitter Command", "Api for Twitter Command to Kafka Producer ", "1.0", "", "rafaelferreiramatos@hotmail.com", "", ""));
 	}
 }
