aae8a40e377f3eb61b43ec7cb3b22d1f104a7c4e
rafael.ferreira <rafah.26ferreira@hotmail.com>
2020-05-15 17:30:16 -0300

    cleanning tests

--- src/test/java/com/dynamic/command/twitter/TwitterClientTest.java
+++ src/test/java/com/dynamic/command/twitter/TwitterClientTest.java
@@ -1,9 +1,11 @@
 package com.dynamic.command.twitter;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
 import java.util.ArrayList;
 import java.util.List;
 
-import static org.junit.Assert.*;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -13,33 +15,32 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 import com.twitter.hbc.core.Client;
 
-
 @RunWith(SpringRunner.class)
 public class TwitterClientTest {
-	
+
 	@InjectMocks
 	private TwitterClient client;
-	
+
 	@Before
 	public void onInit() {
 		MockitoAnnotations.initMocks(this);
 	}
-	
+
 	@Test
 	public void shouldCreateClient() {
 		client.consumerKey = "keyTest";
 		client.consumerSecret = "consumerSecretTest";
 		client.token = "tokenTest";
 		client.secret = "secretTest";
-		
+
 		List<String> listOfTopics = populateTopics();
-		
+
 		Client twitterClient = client.createTwitterClient(listOfTopics);
-		
+
 		assertNotNull(twitterClient);
-		
+
 	}
-	
+
 	@Test
 	public void shouldCreateClientWithTheTopicList() {
 		client.consumerKey = "keyTest";
@@ -49,17 +50,16 @@ public class TwitterClientTest {
 
 		List<String> listOfTopics = populateTopics();
 		String expectedParamString = populatePostParamString(listOfTopics);
-		
-		
+
 		Client twitterClient = client.createTwitterClient(listOfTopics);
-		
+
 		String postParamString = twitterClient.getEndpoint().getPostParamString();
-		
+
 		assertEquals(expectedParamString, postParamString);
 	}
 
 	private String populatePostParamString(List<String> topics) {
-		return "track="+topics.get(0)+"%2C"+topics.get(1)+"%2C"+topics.get(2);
+		return "track=" + topics.get(0) + "%2C" + topics.get(1) + "%2C" + topics.get(2);
 	}
 
 	private List<String> populateTopics() {
